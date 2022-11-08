30afe57cf3c0fb54959d4ea85e46c6d00ce9474c
f <deotadayal27@gmail.com>
2022-11-09 00:57:28 +0530

    d

--- JS/nav.js
+++ JS/nav.js
@@ -1,21 +1,53 @@
+// import {navex} from "/Component/navbar.js"
+// let nav=document.querySelector("#navbar")
+// nav.innerHTML=navex()
+
+// // import {proex} from "/Component/navbar.js"
+// // let prodiv=document.querySelector("#product")
+// // prodiv.innerHTML=proex()
+
+// import {proex} from "/Component/product.js"
+// let prodiv=document.querySelector("#product")
+// prodiv.innerHTML=proex()
+
+
+let nav=document.querySelector("#navbar")
 let prohover = document.querySelector("#prohover");
 let pro = document.querySelector("#product");
 let prices = document.querySelector("#prices");
 let solutions = document.querySelector("#solutions");
 let sol = document.querySelector("#sol");
 let customers = document.querySelector("#customers");
+let res=document.querySelector("#res")
+let partner=document.querySelector("#partner")
+let logo=document.querySelector("#logo")
+
+let resources=document.querySelector("#resources")
+window.onmouseover=()=>{
+    nav.style.position="fixed"
+}
+
 prohover.onmouseover = () => {
+
   pro.style.display = "block";
   pro.style.transition = "0.10ms";
   pro.style.display = "grid";
+  
 };
 prices.onmouseover = () => {
   pro.style.display = "none";
   solutions.style.display = "none";
+  resources.style.display="none"
 };
+partner.onmouseover = () => {
+    pro.style.display = "none";
+    solutions.style.display = "none";
+    resources.style.display="none"
+  };
 customers.onmouseover = () => {
     pro.style.display = "none";
     solutions.style.display = "none";
+    resources.style.display="none"
   };
 pro.onmouseover = () => {
   pro.style.display = "block";
@@ -25,6 +57,11 @@ pro.onmouseover = () => {
 pro.onmouseout = () => {
   pro.style.display = "none";
 };
+logo.onmouseover=()=>{
+    pro.style.display = "none";
+    solutions.style.display = "none";
+    resources.style.display="none"
+}
 
 sol.onmouseover = () => {
   console.log(1);
@@ -41,3 +78,18 @@ solutions.onmouseover = () => {
 solutions.onmouseout = () => {
   solutions.style.display = "none";
 };
+
+res.onmouseover=()=>{
+    resources.style.display = "block"
+  resources.style.transition = "0.10ms"
+  resources.style.display = "grid"
+}
+resources.onmouseover = () => {
+    resources.style.display = "block";
+    resources.style.transition = "0.10ms";
+    resources.style.display = "grid";
+  };
+
+  resources.onmouseout = () => {
+    resources.style.display = "none";
+  };
