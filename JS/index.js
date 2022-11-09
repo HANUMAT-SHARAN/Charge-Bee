452e7d624a767527872574ad9c4efbfbcac07732
amitamitvaghamshi9@gmail.comamitgit config --global user.email amitvaghamshi9@gmail.com <amitvaghamshi9@gmail.comgit config --global user.name amit>
2022-11-09 23:38:08 +0530

    final day2 complete

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,8 @@
+//importing navbar
+import {navbarhtml,producthtml,solutionshtml,resourceshtml} from "../Component/navbar.html.js"
+document.getElementById("navbar").innerHTML=navbarhtml()//,producthtml,solutionshtml,resourceshtml
+
+
 let imagesarr=[`<div class="picture">
 <img src="https://webstatic.chargebee.com/assets/web/535/images/home/coc/customers/jose-bolanos.webp" alt="">
 <div class="pappu">
