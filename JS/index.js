d97b9aa1b2e0d4417027f8164f94da373e8ab66f
amitamitvaghamshi9@gmail.comamitgit config --global user.email amitvaghamshi9@gmail.com <amitvaghamshi9@gmail.comgit config --global user.name amit>
2022-11-11 00:48:54 +0530

    d

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,4 @@
 //import navbar
-<<<<<<< HEAD
  import {navbarhtml,producthtml,solutionshtml,resourceshtml} from "../Component/navbar.html.js";
  let nav = document.querySelector("#navbar");
 
@@ -17,27 +16,6 @@
  import { navbarscript } from "../Scripts/navbarscript.js";
 
  navbarscript();
-=======
-import {navbarhtml,producthtml,solutionshtml,resourceshtml} from "../Component/navbar.js";
-let nav = document.querySelector("#navbar");
-
-let htmlprod = document.querySelector("#product");
-htmlprod.innerHTML = producthtml();
-
-let solut = document.querySelector("#solutions");
-
-solut.innerHTML = solutionshtml();
-
-let reso = document.querySelector("#resources");
-
-reso.innerHTML = resourceshtml();
-
-nav.innerHTML = navbarhtml();
-
-import { navbarscript } from "/Scripts/navbarscript.js";
-
-navbarscript();
->>>>>>> master
 
 let log = document.querySelector("#logo");
 log.onclick = () => {
