9e5e744864569ce3de55137b9aa0a8dc0d405521
Pavan Ingalagi <ingalagipavan@gmail.com>
2022-11-10 13:26:52 +0530

    partnerpage

--- JS/partner.js
+++ JS/partner.js
@@ -1,8 +1,41 @@
+import {
+    navbarhtml,
+    producthtml,
+    solutionshtml,
+    resourceshtml,
+  } from "/Component/navbar.html.js";
+  
+  let nav = document.querySelector("#navbar");
+  
+  let htmlprod = document.querySelector("#product");
+  htmlprod.innerHTML = producthtml();
+  
+  let solut = document.querySelector("#solutions");
+  
+  solut.innerHTML = solutionshtml();
+  
+  let reso = document.querySelector("#resources");
+  
+  reso.innerHTML = resourceshtml();
+  
+  nav.innerHTML = navbarhtml();
+  
+  import { navbarscript } from "/Scripts/navbarscript.js";
+  
+  navbarscript();
+  
+  let log = document.querySelector("#logo");
+  log.onclick = () => {
+    window.location.href = "../HTML/index.html";
+    console.log(1);
+  };
+  let customers = document.querySelector("#customers");
+  customers.onclick = () => {
+    window.location.href = "../HTML/customers.html";
+  };
+  let partner = document.querySelector("#partner");
+  partner.onclick = () => {
+    window.location.href = "../HTML/partner.html";
+    
+  };
 
-let faq = document.getElementById("faq");
-
-faq.onclick = () => {
-    let div = document.createElement("div");
-
-    let h1
-};
